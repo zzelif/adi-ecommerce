c137b42da087a00f2fecb9c952d9618f8761496c
dan <dan.lettac@gmail.com>
2025-07-11 14:39:43 +0800

    prettier plugin and prisma init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next', 'next/core-web-vitals', 'plugin:prettier/recommended'],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  }),
+]
 
 export default eslintConfig;
